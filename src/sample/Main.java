96261eebb6320018cae1069862286596397deaa2
swendolek <wendolek43@gmail.com>
2020-06-15 21:40:21 -0500

    First commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,19 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.*;
-import javafx.scene.control.*;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.stage.*;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     String windowTitle = "Ora";
     Button but;
+
+    ArrayList<Node> objects = new ArrayList<Node>();
 
 
     @Override
@@ -22,12 +23,13 @@ public class Main extends Application {
         but = new Button();
         but.setText("Ha butt");
 
-
-
+        StackPane lay = new StackPane();
+        lay.getChildren().add(but);
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
